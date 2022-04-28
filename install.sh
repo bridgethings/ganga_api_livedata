96bf33af7c3794b2da4b7aeb6bd57d5fec96fff2
BridgeThings <srinivasaraju.namburi@gmail.com>
2022-04-28 13:45:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ pip install flask
 sudo cp /home/pi/swan/flaskApp/flaskApp.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable flaskApp.service
+sudo systemctl start flaskApp.service
