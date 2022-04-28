f1f8a37ee3d42075cacc5f28d2d4ca903bb256c6
BridgeThings <srinivasaraju.namburi@gmail.com>
2022-04-28 13:34:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 cd /home/pi/swan
 wget https://github.com/bridgethings/ganga_api_livedata/archive/refs/heads/main.zip
 unzip main.zip
-cd ganga_api_livedata-main
 mkdir flaskApp
-cp /home/pi/swan/ganga_api_livedata-main/. /home/pi/swan/flaskApp
+cp /home/pi/swan/ganga_api_livedata-main/* /home/pi/swan/flaskApp
 cd flaskApp
 chmod +x install.sh
 ./install.sh
