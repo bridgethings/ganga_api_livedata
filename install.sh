6283991dc31751218dd46311fe0b1a524385b593
teja.vaitla <vaitla.iot@gmail.com>
2022-04-29 15:11:56 +0530

    add template changes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pip install flask
-sudo cp /home/pi/swan/flaskApp/flaskApp.service /etc/systemd/system
+sudo cp -r /home/pi/swan/flaskApp/flaskApp.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable flaskApp.service
 sudo systemctl start flaskApp.service
