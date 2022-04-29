46a9aba311ff2ae05ac2921ff59ab7477868e718
teja.vaitla <vaitla.iot@gmail.com>
2022-04-29 15:18:42 +0530

    add template changes cpy command

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pip install flask
-sudo cp -r /home/pi/swan/flaskApp/flaskApp.service /etc/systemd/system
+sudo cp /home/pi/swan/flaskApp/flaskApp.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable flaskApp.service
 sudo systemctl start flaskApp.service
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ cd /home/pi/swan
 wget https://github.com/bridgethings/ganga_api_livedata/archive/refs/heads/main.zip
 unzip main.zip
 mkdir flaskApp
-cp /home/pi/swan/ganga_api_livedata-main/* /home/pi/swan/flaskApp
+cp -r /home/pi/swan/ganga_api_livedata-main/* /home/pi/swan/flaskApp
 cd flaskApp
 chmod +x install.sh
 ./install.sh
